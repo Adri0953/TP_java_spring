99a5362e4637023d7a36f2e98c5217ba2e911fc5
Andrioaie Adrian <adrian.andrioaie2005@gmail.com>
2025-11-23 15:32:54 +0100

    feat: ajout du endpoint PUT pour update les données d'un utilisateur

--- src/main/java/com/example/accessingdatamysql/MainController.java
+++ src/main/java/com/example/accessingdatamysql/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +35,19 @@ public class MainController {
   @GetMapping(path = "/get/{id}")
   public User getUserById(@PathVariable Integer id) {
     return userRepository.findById(id).orElse(null);
+  }
+
+  @PutMapping(path = "/update/{id}")
+  public User updateUser(@PathVariable Integer id, @RequestBody User userDetails) {
+    return userRepository.findById(id)
+        .map(user -> {
+          user.setUsername(userDetails.getUsername());
+          user.setPassword(userDetails.getPassword());
+          user.setRole(userDetails.getRole());
+          user.setEmail(userDetails.getEmail());
+          user.setName(userDetails.getName());
+          return userRepository.save(user);
+        })
+        .orElse(null);
   }
 }
