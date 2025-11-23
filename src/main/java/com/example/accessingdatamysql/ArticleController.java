5bdeae36ea015b3e34dd898111d7aa8359e724d8
Andrioaie Adrian <adrian.andrioaie2005@gmail.com>
2025-11-23 16:09:06 +0100

    feat: ajout de la classe ArticleController avec endpoints GET all et PUSH un article

---
+++ src/main/java/com/example/accessingdatamysql/ArticleController.java
@@ -0,0 +1,39 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/article")
+public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/add")
+    public String addNewArticle(@RequestParam Integer userId, @RequestParam String title,
+            @RequestParam String content) {
+        User author = userRepository.findById(userId).orElse(null);
+        if (author == null) {
+            return "Utilisateur non trouvé";
+        }
+
+        Article article = new Article(author, title, content);
+        articleRepository.save(article);
+        return "Article créé avec succès";
+    }
+
+    @GetMapping(path = "/all")
+    public Iterable<Article> getAllArticles() {
+        return articleRepository.findAll();
+    }
+}
