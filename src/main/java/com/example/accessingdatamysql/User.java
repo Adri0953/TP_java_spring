e4ab95db5bf1ca6b8e9b98ccad9c889636325a77
Andrioaie Adrian <adrian.andrioaie2005@gmail.com>
2025-11-23 15:10:59 +0100

    feat: Ajouter l'endpoint GET pour récupérer un utilisateur par ID

--- src/main/java/com/example/accessingdatamysql/User.java
+++ src/main/java/com/example/accessingdatamysql/User.java
@@ -8,12 +8,18 @@ import jakarta.persistence.Id;
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
   @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
 
-  private String name;
+  private String username;
+
+  private String password;
+
+  private String role;
 
   private String email;
+
+  private String name;
 
   public Integer getId() {
     return id;
@@ -21,6 +27,38 @@ public class User {
 
   public void setId(Integer id) {
     this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
   }
 
   public String getName() {
@@ -31,11 +69,15 @@ public class User {
     this.name = name;
   }
 
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
+  @Override
+  public String toString() {
+    return "User{" +
+        "id=" + id +
+        ", username='" + username + '\'' +
+        ", password='" + password + '\'' +
+        ", role='" + role + '\'' +
+        ", email='" + email + '\'' +
+        ", name='" + name + '\'' +
+        '}';
   }
 }
